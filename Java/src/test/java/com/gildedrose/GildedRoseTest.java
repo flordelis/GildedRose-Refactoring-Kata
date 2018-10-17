ebed11f685c9e3ec8d0510cee1860ce102b6036f
Helislana D Freitas <helislana.d.freitas@ericssoninovacao.com.br>
2018-10-17 12:25:50 -0300

    Removed others versions [ not JAVA]

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,21 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void updateQuality_forSulfuraItem() {
+        Item[] items = new Item[] {  new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(80, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+    @Test
+    public void updateQuality_forAgedBrie() {
+        Item[] items = new Item[] {  new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
     }
 
 }
